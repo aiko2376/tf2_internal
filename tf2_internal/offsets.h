e0fce90b71bc12dcaaca7a1324ff7a72ae157a04
Luna <78736827+abience@users.noreply.github.com>
2022-01-19 16:12:05 -0600

    Couple more offsets

--- tf2_internal/offsets.h
+++ tf2_internal/offsets.h
@@ -24,4 +24,15 @@ public:
 		// x-ref "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.!:-/%"
 		signature_t finish_drawing = signature_t( "vguimatsurface.dll", "55 8B EC 6A FF 68 DC B1 0C 10 64 A1 ? ? ? ? 50 64 89 25 ? ? ? ? 51 56 6A" );
 	} m_signatures;
+
+	struct {
+		// 0x6 offset from CHLClient::HudUpdate
+		uintptr_t global_vars = 0x6;
+
+		// 0x5 offset from CHLClient::HudProcessInput
+		uintptr_t client_mode = 0x5;
+
+		// 0xA5 offset from CHLClient::Shutdown
+		uintptr_t input = 0xA5;
+	} m_interfaces;
 }; inline c_offsets g_offsets;
