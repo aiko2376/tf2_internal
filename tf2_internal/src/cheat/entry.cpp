7c6894bf9032bae83d3cda1b0a980ff5513211ac
Classy-Code <55356675+Classy-Code@users.noreply.github.com>
2022-01-21 22:23:09 -0500

    here

--- tf2_internal/src/cheat/entry.cpp
+++ tf2_internal/src/cheat/entry.cpp
@@ -1,6 +1,14 @@
-#include "sdk.h"
+#include "../sdk/sdk.h"
 
 unsigned long __stdcall cheat_thread( _In_ LPVOID reserved ) {
+
+#ifdef _DEBUG
+	AllocConsole( );
+	SetConsoleTitleA( xor("TF2 Internal" ));
+	freopen_s( reinterpret_cast< FILE** >( stdout ), xor("CONOUT$"), xor("w"), stdout );
+	g_utils.log( "[DEBUG]: Injected\n" );
+#endif
+
 	g_client.init( );
 
 	while ( !( GetAsyncKeyState( VK_DELETE ) & 1 ) )
@@ -9,12 +17,11 @@ unsigned long __stdcall cheat_thread( _In_ LPVOID reserved ) {
 	g_client.unload( );
 
 	FreeLibraryAndExitThread( static_cast< HMODULE >( reserved ), 0 );
-	return 0;
 }
 
 BOOL WINAPI DllMain( _In_ HINSTANCE instance, _In_ DWORD reason, _In_ LPVOID reserved ) {
 	if ( reason == DLL_PROCESS_ATTACH ) {
-		if ( auto* const thread_handle = CreateThread( nullptr, 0, cheat_thread, instance, 0, nullptr ) )
+		if ( auto* const& thread_handle = CreateThread( nullptr, 0, cheat_thread, instance, 0, nullptr ); thread_handle )
 			CloseHandle( thread_handle );
 	}
 
---
+++ tf2_internal/src/sdk/sdk.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Windows.h>
+#include <chrono>
+#include <thread>
+
+#include "../cheat/utils/minhook/minhook.h"
+
+#include "../cheat/utils/utils.h"
+#include "../cheat/utils/xor.h"
+#include "offsets.h"
+
+#include "../cheat/hack/client.h"
