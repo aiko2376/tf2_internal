3251ee4c093ad22ab5182655682c2d0370400831
Princess-Lunaa <78736827+abience@users.noreply.github.com>
2022-03-13 05:45:00 -0500

    Paint hooked

--- tf2 internal/hooks.cpp
+++ tf2 internal/hooks.cpp
@@ -1,9 +1,17 @@
 #include "sdk.h"
 
-void c_hooks::init( ) {
+void __fastcall paint( std::uintptr_t ecx, std::uintptr_t edx, paint_mode_t mode ) {
+	g_hooks.m_original.paint( ecx, edx, mode );
+}
 
+void c_hooks::init( ) {
+	MH_Initialize( );
+	MH_CreateHook( c_utils::get_virtual_function<void*>( g_interfaces.m_engine_vgui, e_indexes::PAINT ), paint, reinterpret_cast< void** >( &this->m_original.paint ) );
+	MH_EnableHook( MH_ALL_HOOKS );
 }
 
 void c_hooks::unload( ) {
-
+	MH_DisableHook( MH_ALL_HOOKS );
+	MH_RemoveHook( MH_ALL_HOOKS );
+	MH_Uninitialize( );
 }
--- tf2 internal/hooks.h
+++ tf2 internal/hooks.h
@@ -1,7 +1,15 @@
 #pragma once
 
 class c_hooks {
+	enum e_indexes {
+		PAINT = 13
+	};
 public:
+	struct {
+		typedef void( __fastcall* paint_fn )( std::uintptr_t, std::uintptr_t, paint_mode_t );
+		paint_fn paint;
+	} m_original;
+
 	void init( );
 	void unload( );
 }; inline c_hooks g_hooks;
